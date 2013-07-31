d3b707eea40a8dfc08d6e53400d9cb8386ab083a
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-31 12:21:51 +0900

    fix: boot order and stop /etc/sysconfig/kickstart

--- install-rhl.sh
+++ install-rhl.sh
@@ -4,7 +4,6 @@
 install -m 755 usr/sbin/xe-set-authorized-keys /usr/sbin/xe-set-authorized-keys
 install -m 755 usr/sbin/xe-set-hostname /usr/sbin/xe-set-hostname
 install -m 755 usr/sbin/xe-set-network-rpm /usr/sbin/xe-set-network
-install -m 755 usr/sbin/generate-sshd-keys /usr/sbin/generate-sshd-keys
 
 install -m 755 etc/init.d/kickstart /etc/init.d/kickstart
 install -m 755 etc/init.d/xe-automater /etc/init.d/xe-automater
@@ -12,4 +11,5 @@ install -m 755 etc/init.d/xe-automater /etc/init.d/xe-automater
 # set service
 /sbin/chkconfig kickstart on
 /sbin/chkconfig xe-automater off
+
 
