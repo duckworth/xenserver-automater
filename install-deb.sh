39417fa1604e2927f0cc31155681ca348e80f037
John Lemp <john@graavy.com>
2015-04-12 08:14:05 -0400

    more filename typos

--- install-deb.sh
+++ install-deb.sh
@@ -6,7 +6,7 @@ apt-get install -y xenstore-utils
 cp usr/sbin/xe-set-authorized-keys /usr/sbin/xe-set-authorized-keys
 cp usr/sbin/xe-set-hostname /usr/sbin/xe-set-hostname
 cp usr/sbin/xe-set-network-deb /usr/sbin/xe-set-network
-cp etc/init/xe-automator.conf /etc/init/xe-automater.conf
+cp etc/init/xe-automater.conf /etc/init/xe-automater.conf
 chmod a+x /usr/sbin/xe-set-authorized-keys
 chmod a+x /usr/sbin/xe-set-hostname
 chmod a+x /usr/sbin/xe-set-network
